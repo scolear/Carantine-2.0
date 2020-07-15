4c07e2ae24e85b2deb52628634a3a1b694c042be
scolear <bobtera@gmail.com>
2020-07-15 09:43:34 +0200

    initial commit

---
+++ CarantineAngularJS/app/app.js
@@ -0,0 +1,30 @@
+﻿const app = angular.module('CarantineApp', []);
+
+app.controller('CarantineController', function () {
+
+    this.state = "STOPPED";
+
+    this.up = function () { console.log("up"); };
+    this.down = function () { console.log("down"); };
+    this.left = function () { console.log("left"); };
+    this.right = function () { console.log("right"); };
+    this.stop = function () { console.log("stop"); };
+
+    // TODO: values to be updated from actual readings.
+    this.sensors = [
+        {
+            pos: "left",
+            value: 12.34
+        },
+        {
+            pos: "center",
+            value: 56.78
+        },
+        {
+            pos: "right",
+            value: 90.12
+        }
+    ];
+
+
+});
